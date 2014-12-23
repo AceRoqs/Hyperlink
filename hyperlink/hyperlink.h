9b2b87235b9dd14f71c1c39149bafe2ebb8b425b
Toby Jones <ace_roqs@yahoo.com>
2014-12-23 12:47:24 -0800

    Remove linkage specification so namespaces can be used.

--- hyperlink/hyperlink.h
+++ hyperlink/hyperlink.h
@@ -1,17 +1,9 @@
 #pragma once
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 // Reference hyperlink_control_class when using CreateWindow to create
 // a hyperlink control.
 const PCTSTR hyperlink_control_class = TEXT("Hyperlink_control_class");
 
 HRESULT register_hyperlink_class(_In_ HINSTANCE instance);
 void unregister_hyperlink_class(_In_ HINSTANCE instance);
-
-#ifdef __cplusplus
-}
-#endif
 
